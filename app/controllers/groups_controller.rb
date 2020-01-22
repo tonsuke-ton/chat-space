5fdf4bdd1e6333653e5f3bcf2304d4dc39cfb98e
tonsuke-ton <koko.twi.kaza@gmail.com>
2020-01-22 20:48:20 +0900

    修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
-    
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -19,7 +19,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
@@ -34,6 +33,10 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
