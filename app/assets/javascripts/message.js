12f67db42325a32165c84c404a06f690f722de9a
tonsuke-ton <koko.twi.kaza@gmail.com>
2020-01-27 21:21:57 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,6 @@ $(function(){
       $('.messages').append(html);      
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $(".submit-btn").prop("disabled", false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
